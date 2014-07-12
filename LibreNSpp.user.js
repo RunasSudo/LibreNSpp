2cc645751183a341fe26f64f2d3bb4bb25be55ed
RunasSudo <RunasSudo@gmail.com>
2014-07-12 14:39:23 +0930

    Added infinite scroll.
    Tada!

--- LibreNSpp.user.js
+++ LibreNSpp.user.js
@@ -10,12 +10,47 @@
 // @copyright  2014, RunasSudo
 // ==/UserScript==
 
-alert("Hello");
+//Infinite RMB scroll
+if (window.location.href.indexOf("/region=") >= 0) { //Are we on the RMB page?
+    rmb = $(".rmbtable2");
+    rmb.children().each(function(i,entry){rmb.prepend(entry)}); //Reverse order so newest are at top.
+    $(".rmbolder").hide(); //GO AWAI!
+    
+    //Add scroll detector
+    $("<div id=\"infiniteScroll\" style=\"border: 1px #CCC solid; border-radius: 12px; -moz-border-radius: 12px; -webkit-border-radius: 12px; margin-top: 4px; margin-bottom: 4px; padding: 0 8px 0 12px; background-color: #FDFFFC; text-align: center; font-weight: bold; margin-left: 18%; margin-right: 18%; min-height: 18px; color: #AAA;\"></div>")
+    .html("Infinite Scroll!")
+    .insertAfter(rmb.parent());
+    
+    infiniteScroll();
+}
 
-GM_xmlhttpRequest({
-  method: "GET",
-  url: "http://www.nationstates.net/cgi-bin/api.cgi?nation=south_jarvis",
-  onload: function(response) {
-    alert(response.responseText);
-  }
-});
+//LibreNS++ functions
+var rmbOffset = 0;
+function infiniteScroll() {
+    if (isScrolledIntoView("#infiniteScroll")) {
+        //Load new RMB messages
+        $("#infiniteScroll").html("Loading&hellip;");
+        rmbOffset += 10;
+        $.get("/page=ajax/a=rmb/region=" + "democratic_socialist_assembly" + "/offset=" + rmbOffset, function(data) {
+            if (data.length > 1) {
+                $($(data).get().reverse()).insertAfter(".rmbrow:last");
+                $("#infiniteScroll").html("Infinite Scroll!");
+            } else {
+                $("#infiniteScroll").html("At earliest message.");
+            }
+        });
+    }
+    
+    setTimeout(infiniteScroll, 1000);
+}
+
+//Utility functions
+function isScrolledIntoView(elem) { //https://stackoverflow.com/questions/487073/check-if-element-is-visible-after-scrolling
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+    
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+    
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
