2113a3cfbc48b9b2716b17633ceeffa784086eef
RunasSudo <RunasSudo@gmail.com>
2014-11-23 15:21:21 +1030

    Switch to m4

---
+++ src/LibreNSpp.user.js
@@ -0,0 +1,8 @@
+m4_include(header.js)
+m4_include(pageAll.js)
+m4_include(puppets.js)
+m4_include(pageRegion.js)
+m4_include(pageNewspaper.js)
+m4_include(pageNewspaperEditor.js)
+m4_include(pageRegionalDispatch.js)
+m4_include(basic.js)
--- src/build.sh
+++ src/build.sh
@@ -8,11 +8,4 @@ js-beautify -r -f pageNewspaperEditor.js
 js-beautify -r -f pageRegionalDispatch.js
 js-beautify -r -f basic.js
 
-cat header.js > ../LibreNSpp.user.js
-cat pageAll.js >> ../LibreNSpp.user.js
-cat puppets.js >> ../LibreNSpp.user.js
-cat pageRegion.js >> ../LibreNSpp.user.js
-cat pageNewspaper.js >> ../LibreNSpp.user.js
-cat pageNewspaperEditor.js >> ../LibreNSpp.user.js
-cat pageRegionalDispatch.js >> ../LibreNSpp.user.js
-cat basic.js >> ../LibreNSpp.user.js
+m4 -P LibreNSpp.user.js>../LibreNSpp.user.js
