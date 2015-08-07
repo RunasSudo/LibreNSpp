aaf6b0bcd81370c892fd4174427ddba9fb6bf67c
RunasSudo <RunasSudo@gmail.com>
2015-08-07 16:11:23 +0930

    Prevent jQuery 2.1.1 messing with NS jQuery 1.x. Fix #44.

--- src/LibreNSpp.user.js
+++ src/LibreNSpp.user.js
@@ -1,6 +1,8 @@
 m4_include(header.js)
 
 m4_include(../include/jquery-2.1.1.min.js)
+this.$ = this.jQuery = jQuery.noConflict(true); //Prevent jQuery 2.1.1 messing with NS jQuery 1.x.
+
 m4_include(../include/jquery.linkify.min.js)
 m4_include(../include/json2.min.js)
 m4_include(compat.js)
