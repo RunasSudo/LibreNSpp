ff8a8dddff7d23d717f95982f1a99000bdc43e8b
Viko <viko@vikomprenas.com>
2015-10-06 20:50:04 -0600

    Added Subscribed and Bookmarked links to the sidebar

--- src/pageAll.js
+++ src/pageAll.js
@@ -19,5 +19,10 @@ function allPage() {
     
     //--------------------
     //Footer note
-    $("#footbar").append('  &middot;  <a href="https://forum.nationstates.net/viewtopic.php?f=15&t=304199">LibreNS++</a> ' + version + '!')
+    $("#footbar").append('  &middot;  <a href="https://forum.nationstates.net/viewtopic.php?f=15&t=304199">LibreNS++</a> ' + version + '!');
+    
+    //--------------------
+    //Sidebar
+    $(".panelcontent .menu li:nth-child(5) ul.popoutmenu").append('<li><a href="//forum.nationstates.net/ucp.php?i=main&mode=subscribed"><i class="icon-radar"></i>Subscribed</a></li>')
+                                                          .append('<li><a href="//forum.nationstates.net/ucp.php?i=main&mode=bookmarks"><i class="icon-book"></i>Bookmarked</a></li>');
 }
