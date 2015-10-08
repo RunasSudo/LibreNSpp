0926b449489b203dab542c2bd96f81b773f86843
Viko <viko@vikomprenas.com>
2015-10-08 14:44:45 -0600

    Made a simple notification system (call signal())

--- src/notify.js
+++ src/notify.js
@@ -5,3 +5,11 @@ function loadNotifySound() {
 function notifySound() {
     $("audio#librenspp-notification-sound").get(0).play();
 }
+
+function signal() {
+    if (rift) {
+        if ($("#librenspp .belcontent .notificationnumber").length == 0) {
+            $("#librenspp .belcontent").append('<div style="font-weight: 700;" class="notificationnumber">!</div>');
+        }
+    }
+}
