54740d7c626ed4d9031182b8be8dd83b1508227d
Viko <viko@vikomprenas.com>
2015-10-08 09:09:41 -0600

    First, an honest mistake... id, not class In my defence it's a one-pixel image...

--- src/cosmetic.js
+++ src/cosmetic.js
@@ -2,7 +2,7 @@ function cosmetic() {
     $(".panelcontent").css("margin-left", "0px")
                       .css("border-top-left-radius", "0px")
                       .css("border-bottom-left-radius", "0px");
-    $(".accessiblitylink").hide(); // This is a typo in NS, don't change it unless NS changes it
+    $("#accessiblitylink").hide(); // This is a typo in NS, don't change it unless NS changes it
 }
 
 // This messes with the footer a little bit on pages that are shorter than your screen height.
