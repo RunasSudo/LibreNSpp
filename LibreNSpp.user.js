e1c282523768cc33c4bb6006832b01a040e26991
Viko <viko@vikomprenas.com>
2015-10-08 17:16:02 -0600

    Another minor cosmetic adjustment

--- LibreNSpp.user.js
+++ LibreNSpp.user.js
@@ -124,6 +124,7 @@ function cosmetic() {
                       .css("border-top-left-radius", "0px")
                       .css("border-bottom-left-radius", "0px");
     $("#accessiblitylink").hide(); // This is a typo in NS, don't change it unless NS changes it
+    $("#nationcover").css("box-shadow", "0px 4px 4px 0px rgba(0, 0, 0, 0.5)");
 }
 
 // This messes with the footer a little bit on pages that are shorter than your screen height.
@@ -170,7 +171,7 @@ function allPage() {
     //Sidebar
     $(".panelcontent .menu li:nth-child(5) ul.popoutmenu").append('<li><a href="//forum.nationstates.net/ucp.php?i=main&mode=subscribed"><i class="icon-radar"></i>Subscribed</a></li>')
                                                           .append('<li><a href="//forum.nationstates.net/ucp.php?i=main&mode=bookmarks"><i class="icon-book"></i>Bookmarked</a></li>');
-    //
+    // (to fix the indenting, leave this here)
     
     //--------------------
     //Check for update
