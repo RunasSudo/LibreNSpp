54878b6b39512632dcd22d2b818ae943d51d0a71
Viko <viko@vikomprenas.com>
2016-01-03 17:32:26 -0600

    nope, can't get that to work

--- src/pageRegion.js
+++ src/pageRegion.js
@@ -134,7 +134,7 @@ function updateRMB() { //Triggered at intervals. Looks for live RMB updates.
                                        .replace(/<br>/g, "\n").replace(/<\/p><p>/g, "\n")
                                        .slice(3).replace(/<[^>]*>/g, "")
                                        .replace(/LikeQuoteSuppress/g, "").replace(/LikeQuote/g, "").trim();
-                        var quotee = $(post).find(".rmbquoted a.nlink span").text();
+                        var quotee = $(post).find(".rmbquoted legend a.nlink span").text();
                         notifyDesktop(poster, (quotee !== "" ? "(quoting " + quotee + ") " : "") + content);
                     }
                 } else {
